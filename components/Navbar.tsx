14c7f0f5d88c587addf672e085fae35b64d90e25
Ayush Mehta <ayushmehtabps@gmail.com>
2025-07-20 17:12:35 +0530

    component add

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import logo from "../public/logo.png"
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between mx-auto w-full px-14 py-4 max-sm:px-4'>
+ <Link href="/">
+ <div className='flex items-center gap-2.5 cursor-pointer'>
+<Image src={logo} alt='logo' width={46} height={44} 
+/>
+
+ </div>
+ </Link>
+<div className='flex items-center gap-8'>
+<p>Home</p>
+<p>Companions</p>
+<p>My Journey</p>
+<p>Sign In</p>
+</div>
+    </nav>
+  )
+}
+
+export default Navbar
